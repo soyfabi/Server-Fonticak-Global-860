0f462603f3d9513005b6e782a5d9e17e8ce598e8
soyfabi <fabi.marzan1762@yahoo.com>
2023-05-11 18:19:42 -0400

    Update Achievements and creature

--- data/scripts/eventcallbacks/player/default_onMoveItem.lua
+++ data/scripts/eventcallbacks/player/default_onMoveItem.lua
@@ -72,13 +72,13 @@ event.onMoveItem = function(self, item, count, fromPosition, toPosition, fromCyl
 	-- Bath tube
 	local toTile = Tile(toCylinder:getPosition())
 	local topDownItem = toTile:getTopDownItem()
-	if topDownItem and table.contains({ 29312, 29313 }, topDownItem:getId()) then
+	if topDownItem and table.contains({ BATHTUB_EMPTY, BATHTUB_FILLED }, topDownItem:getId()) then
 		return false
 	end
 	
 	-- SSA exhaust
 	if toPosition.x == CONTAINER_POSITION and toPosition.y == CONST_SLOT_NECKLACE
-	and item:getId() == 2197 then
+	and item:getId() == 3081 then
 		local pid = self:getId()
 		if stoneSkinAmuletExhausted[pid] then
 			addEvent(function()self:sendCancelMessage("Wait 2 seconds for equip SSA again.") end, 100)
@@ -144,13 +144,13 @@ event.onMoveItem = function(self, item, count, fromPosition, toPosition, fromCyl
 	local containerTo = self:getContainerById(toPosition.y-64)
 	if (containerTo) then
 	-- Gold Pouch
-		if (containerTo:getId() == 26377) then
+		if (containerTo:getId() == 23721) then
 			if (not (item:getId() == ITEM_CRYSTAL_COIN or item:getId() == ITEM_PLATINUM_COIN or item:getId() == ITEM_GOLD_COIN)) then
 				self:sendTextMessage(MESSAGE_INFO_DESCR, "You can move only money to this container.")
 				return false
 			end
 		end
-	local potions = {7618, 7588, 7591, 8473, 7620, 7589, 7590, 8472}
+	local potions = {268, 237, 238, 266, 236, 239, 7643, 23375, 7642, 23374}
 	local runes = {2260, 2261, 2262, 2263, 2264, 2265, 2266, 2267, 2268, 2269, 2270, 2271, 2272, 2273, 2274, 2275, 2276, 2277, 2278, 2279, 2280, 2281, 2282, 2283, 2284, 2285, 2286, 2287, 2288, 2289, 2290, 2291, 2292, 2293, 2294, 2295, 2296, 2297, 2298, 2299, 2300, 2301, 2302, 2303, 2304, 2305, 2306, 2307, 2308, 2309, 2310, 2311, 2312, 2313, 2314, 2315, 2316}
 		--Supply Stash
 		if (containerTo:getId() == ITEM_SUPPLY_STASH) then
@@ -175,7 +175,7 @@ event.onMoveItem = function(self, item, count, fromPosition, toPosition, fromCyl
 			self:sendCancelMessage('Sorry, not possible.')
 			return false
 		end
-
+	
 		-- The player also shouldn't be able to insert items into the boss corpse		
 		local tile = Tile(container:getPosition())
 		for _, item in ipairs(tile:getItems()) do
