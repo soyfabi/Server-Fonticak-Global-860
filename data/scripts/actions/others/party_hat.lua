d535311dcf95048c54983e2d54609674443acad2
soyfabi <fabi.marzan1762@yahoo.com>
2023-05-11 14:03:08 -0400

    MoveEvents to RevScripts

--- data/scripts/actions/others/party_hat.lua
+++ data/scripts/actions/others/party_hat.lua
@@ -14,7 +14,6 @@ function partyHat.onUse(player, item, fromPosition, target, toPosition, isHotkey
 	
 	local slot = player:getSlotItem(CONST_SLOT_HEAD)
 	if slot and item.uid == slot.uid then
-		player:addAchievementProgress('Party Animal', 200)
 		player:getPosition():sendMagicEffect(CONST_ME_GIFT_WRAPS)
 		exhaust[playerId] = currentTime + exhaustTime
 		return true
